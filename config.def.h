fd1dd215e2132310cad8b651c52d387da38a74c3
EinYakAmNil <einyak@gmail.com>
2022-10-01 23:04:46 +0200

    add font for emojis

--- config.def.h
+++ config.def.h
@@ -6,6 +6,11 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Noto Color Emoji:style=Regular:pixelsize=18"
+};
+
 static int borderpx = 2;
 
 /*
